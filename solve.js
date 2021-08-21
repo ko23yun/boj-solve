c3a7d7e58f583dc6d4ec15d3434be7b3fa0cca9f
ko23yun <ko23yun@naver.com>
2021-08-21 15:58:27 +0900

    boj 1315 참고

--- solve.js
+++ solve.js
@@ -1,15 +1,30 @@
 // 문제 풀고 나서 add, commit 해주기
+// https://velog.io/@exploit017/%EB%B0%B1%EC%A4%80Node.js-1316%EB%B2%88-%EA%B7%B8%EB%A3%B9-%EB%8B%A8%EC%96%B4-%EC%B2%B4%EC%BB%A4 참고...
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString();
+let input = fs.readFileSync("./input.txt").toString().split("\n");
 
-let word = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+let N = Number(input[0]);
+let countGroupWord = 0;
 
-// word 단어 있는지 전체 확인, 그리고 제거!
+for (let i = 1; i <= N; i++) {
+  const word = input[i];
+  const letter = [];
+  let isGroupWord = true;
 
-let count = 0;
+  for (let j = 0; j < input[i]; j++) {
+    if (letter.indexOf(word[j]) === -1) {
+      letter.push(word[j]);
+    } else {
+      if (letter.indexOf(word[j]) !== letter.length - 1) {
+        isGroupWord = false;
+        break;
+      }
+    }
+  }
 
-for (let i = 0; i < word.length; i++) {
-  if (input.indexOf(word[i]) > -1) {
-    count++;
+  if (isGroupWord) {
+    countGroupWord += 1;
   }
 }
+
+console.log(countGroupWord);
