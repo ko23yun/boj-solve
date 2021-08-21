8f83240356a4be607c73811d7d3c486d507f014f
ko23yun <ko23yun@naver.com>
2021-08-21 21:50:02 +0900

    boj 2941 fail

--- solve.js
+++ solve.js
@@ -1,54 +1,28 @@
 // 문제 풀고 나서 add, commit 해주기
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().toUpperCase();
+let input = fs.readFileSync("./input.txt").toString();
 
-let arr = [];
+let word = ["dz=", "c=", "c-", "d-", "lj", "nj", "s=", "z="];
 
-arr.push(input[0]);
+// 반복문으로 "dz=" 부터 (input.length - 3) 번 만큼 찾는다.
+// 그리고 나머지는 다 2번 씩
 
-for (let i = 1; i < input.length; i++) {
-  for (let j = 0; j < arr.length; j++) {
-    // input[i] 가 arr에 들어있는지 확인!!
-    if (arr.indexOf(input[i]) == -1) {
-      arr.push(input[i]);
+for (let i = 0; i < word.length; i++) {
+  // "dz=" 는 (input.length - 2) 만큼 계산하고
+  // 나머지 word는 (input.length - 1) 만큼 계산한다.
+  if (word[i].length == 3) {
+    for (let j = 0; j < input.length - 2; j++) {
+      if (word[i] == input.substring(j, j + 3)) {
+        input = input.replace(input.substring(j, j + 3), 1);
+      }
+    }
+  } else {
+    for (let k = 0; k < input.length - 1; k++) {
+      if (word[i] == input.substring(k, k + 2)) {
+        input = input.replace(input.substring(k, k + 2), 1);
+      }
     }
   }
 }
 
-let newArr = [];
-
-for (let i = 0; i < arr.length; i++) {
-  let count = 0;
-
-  for (let j = 0; j < input.length; j++) {
-    if (arr[i] == input[j]) {
-      count++;
-    }
-  }
-
-  newArr.push(count);
-}
-
-let max = newArr[0];
-
-for (let i = 0; i < newArr.length; i++) {
-  if (max < newArr[i]) {
-    max = newArr[i];
-  }
-}
-
-let countMax = 0;
-
-for (let i = 0; i < newArr.length; i++) {
-  if (max == newArr[i]) {
-    countMax++;
-  }
-}
-
-if (countMax > 1) {
-  // 최댓값이 겹치는 경우
-  console.log("?");
-} else if (countMax == 1) {
-  // 최댓값이 하나인 경우
-  console.log(arr[newArr.indexOf(max)]);
-}
+console.log(input.length);
