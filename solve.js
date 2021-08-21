ba5ef4cdf61bdda888bfa598853338e468f92fbe
ko23yun <ko23yun@naver.com>
2021-08-21 13:33:49 +0900

    boj 1157 fail...

--- solve.js
+++ solve.js
@@ -1,31 +1,19 @@
 // 문제 풀고 나서 add, commit 해주기
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().split("\n");
+const { isRegExp } = require("util");
+let input = fs
+  .readFileSync("./input.txt")
+  .toString()
+  .split("")
+  .map((v) => v.toUpperCase());
 
-// 5번 동안 각 케이스의 평균을 구한다.
-// 평균이 넘는 학생의 비율을 반올림해서 소수점 셋째 자리까지 출력
+// for loop 사용해서 indexof 로 값이 같다면 account에 추가해주기
+// 출력은 반드시 대문자
 
-let C = Number(input[0]);
-let sumArr = [];
+let arr = [];
 
-for (let i = 1; i <= C; i++) {
-  // 일단 각 케이스 배열로 만들기
-  let arr = input[i].split(" ").map((v) => +v);
-
-  let sum = 0;
-  let account = 0;
-
-  for (let i = 1; i < arr.length; i++) {
-    sum += arr[i];
-  }
-
-  for (let i = 1; i < arr.length; i++) {
-    if (sum / arr[0] < arr[i]) {
-      account++;
-    }
-  }
-
-  let per = (account / arr[0]) * 100;
-
-  console.log(`${per.toFixed(3)}%`);
+for (let i = 0; i < input.length; i++) {
+  arr.push(input.filter((x) => input[i] === x));
 }
+
+console.log(arr);
