6420358e86ee5f8d9b3160381d2f9a5de482c60f
ko23yun <ko23yun@naver.com>
2021-09-02 14:47:54 +0900

    boj 2581 소수 찾기

--- solve.js
+++ solve.js
@@ -1,10 +1,36 @@
 // 문제 풀고 나서 add, commit 해주기
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().split(" ");
+const input = fs
+  .readFileSync("./input.txt")
+  .toString()
+  .split("\n")
+  .map((v) => +v);
 
-let a = BigInt(input[0]);
-let b = BigInt(input[1]);
+let minPrime = 0;
+let countPrime = 0;
 
-let answer = a + b;
+function addPrimeNumbes(n) {
+  if (n < 2) {
+    return;
+  }
 
-console.log(answer.toString());
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return;
+    }
+  }
+
+  if (minPrime === 0) {
+    minPrime = n;
+  }
+
+  countPrime += n;
+}
+
+for (let i = input[0]; i <= input[1]; i++) {
+  addPrimeNumbes(i);
+}
+
+let answer = countPrime + "\n" + minPrime;
+
+console.log(minPrime !== 0 ? answer : -1);
