a8196d19414b2a80ed344b0a578df357d706c9b7
ko23yun <ko23yun@naver.com>
2021-09-06 20:45:53 +0900

    boj 3009 풀이 완료

--- solve.js
+++ solve.js
@@ -1,18 +1,35 @@
 let fs = require("fs");
-let input = fs
-  .readFileSync("./input.txt")
-  .toString()
-  .trim()
-  .split(" ")
-  .map((v) => +v);
+let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-let [x, y, w, h] = input;
-let distance = [];
+let xValue = [];
+let yValue = [];
 
-distance.push(w - x, x, h - y, y);
+for (let i = 0; i < input.length; i++) {
+  xValue.push(input[i].split(" ")[0]);
+  yValue.push(input[i].split(" ")[1]);
+}
 
-distance.sort((a, b) => {
-  return a - b;
-});
+// xValue, yValue 안에서 가장 적게 쓰인 값만 구하면 된다.
+xValue.sort((a, b) => a - b);
+yValue.sort((a, b) => a - b);
 
-console.log(distance[0]);
+// 첫번째 값과 나머지 2개의 값을 비교,, 총 3가지 경우의 수가 있음
+let x = xValue[0];
+if (x === xValue[1] && x !== xValue[2]) {
+  x = xValue[2];
+} else if (x !== xValue[1] && x === xValue[2]) {
+  x = xValue[1];
+} else {
+  x = xValue[0];
+}
+
+let y = yValue[0];
+if (y === yValue[1] && y !== yValue[2]) {
+  y = yValue[2];
+} else if (y !== yValue[1] && y === yValue[2]) {
+  y = yValue[1];
+} else {
+  y = yValue[0];
+}
+
+console.log(x, y);
