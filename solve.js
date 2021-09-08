b3375c03b9b712e025b3bd7f80d1b903dc254c42
ko23yun <ko23yun@naver.com>
2021-09-08 21:57:25 +0900

    boj 2447 푸는 중

--- solve.js
+++ solve.js
@@ -2,11 +2,29 @@
 let fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim();
 
-let N = Number(input);
+// input을 Number로 바꿈
+input = Number(input);
 
-function factorial(n) {
-  if (n <= 1) return 1;
-  return n * factorial(n - 1);
+function num(tri) {
+  let i = 1;
+  while (Math.pow(3, i) < tri) {
+    i++;
+  }
+  return i;
 }
 
-console.log(factorial(N));
+// input의 거듭제곱근을 구함
+let repeatedNum = num(input);
+
+// 일단 N이 3일 때 출력 모습을 갖추고 그리고 재귀로 반복
+// function star (n) {
+//   for (let i = 0; i < )
+// }
+
+let star = "*";
+
+for (let i = 0; i < 1; i++) {
+  for (let j = 0; j < 3; j++) {
+    console.log(star);
+  }
+}
