3b1468d76bcf80a5b4f3471239958a5a100616fd
ko23yun <ko23yun@naver.com>
2021-09-10 11:31:32 +0900

    boj 브루트포스 2231 완료

--- solve.js
+++ solve.js
@@ -2,29 +2,23 @@
 let fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim();
 
-// input을 Number로 바꿈
-input = Number(input);
+let N = Number(input);
+let arr = [];
 
-function num(tri) {
-  let i = 1;
-  while (Math.pow(3, i) < tri) {
-    i++;
+for (let i = 1; i < N; i++) {
+  let answer = i;
+  for (let j = 0; j < String(i).length; j++) {
+    answer += Number(String(i)[j]);
   }
-  return i;
+
+  if (answer === N) {
+    arr.push(i);
+    // console.log(i);
+    console.log(arr[0]);
+    break;
+  }
 }
 
-// input의 거듭제곱근을 구함
-let repeatedNum = num(input);
-
-// 일단 N이 3일 때 출력 모습을 갖추고 그리고 재귀로 반복
-// function star (n) {
-//   for (let i = 0; i < )
-// }
-
-let star = "*";
-
-for (let i = 0; i < 1; i++) {
-  for (let j = 0; j < 3; j++) {
-    console.log(star);
-  }
+if (arr.length === 0) {
+  console.log(0);
 }
