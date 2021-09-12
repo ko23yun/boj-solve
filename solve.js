f6e39e5fbf925318ba5f95a3234d4308202f1f0e
ko23yun <ko23yun@naver.com>
2021-09-12 17:57:29 +0900

    boj 2108 최빈값.. 못품..

--- solve.js
+++ solve.js
@@ -1,27 +1,47 @@
 // add, commit, push 해주기!!!
 let fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim();
+let input = fs
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => +v);
 
-let N = Number(input);
-let sixValue = [];
-// 666부터 모든 수를 비교해서 2번째 666이 들어가는 수를 뽑아야하겠네
+let N = input.shift();
 
-function tripleSix(n) {
-  let num = String(n);
+// 산술평균 : N개의 수들의 합을 N으로 나눈 값
+// 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+// 최빈값 : N개의 수들 중 가장 많이 나타나는 값
+// 범위 : N개의 수들 중 최댓값과 최솟값의 차이
 
-  for (let i = 0; i < num.length - 2; i++) {
-    if (num[i] === "6" && num[i + 1] === "6" && num[i + 2] === "6") {
-      sixValue.push(num);
+let result = [];
+
+function stat(arr) {
+  // 산술평균
+  let sum = 0;
+  for (let i = 0; i < N; i++) {
+    sum += arr[i] / N;
+  }
+  result.push(Math.round(sum));
+  // 중앙값
+  let centerArr = input;
+  centerArr.sort((a, b) => a - b);
+  result.push(centerArr[Math.floor(N / 2)]);
+  // 최빈값
+  // 셋째 줄에는 최빈값을 출력한다. 여러 개 있을 때에는 최빈값 중 두 번째로 작은 값을 출력한다.
+  let map = new Map();
+  for (let i = 0; i < N; i++) {
+    if (!map.has(input[i])) {
+      map.set(input[i], 1);
+    } else {
+      map.set(input[i], map.get(input[i]) + 1);
     }
   }
+  console.log(map.entries());
+  // 범위
+  result.push(input[input.length - 1] - input[0]);
 }
 
-let n = 666;
+stat(input);
 
-while (sixValue.length < N) {
-  tripleSix(n);
-
-  n++;
-}
-
-console.log(sixValue[sixValue.length - 1]);
+console.log(result);
