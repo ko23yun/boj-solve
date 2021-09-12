985af93076bef835cb2859bb2f2b336ade4a0b44
ko23yun <ko23yun@naver.com>
2021-09-12 20:15:21 +0900

    boj 1181 완료,, 근데 시간제한에 아슬아슬하게 통과,, 다시 확인 필요

--- solve.js
+++ solve.js
@@ -1,47 +1,28 @@
 // add, commit, push 해주기!!!
 let fs = require("fs");
-let input = fs
-  .readFileSync("./input.txt")
-  .toString()
-  .trim()
-  .split("\n")
-  .map((v) => +v);
+let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-let N = input.shift();
+let N = Number(input.shift());
+let map = new Map();
+let newArr = [];
 
-// 산술평균 : N개의 수들의 합을 N으로 나눈 값
-// 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
-// 최빈값 : N개의 수들 중 가장 많이 나타나는 값
-// 범위 : N개의 수들 중 최댓값과 최솟값의 차이
-
-let result = [];
-
-function stat(arr) {
-  // 산술평균
-  let sum = 0;
-  for (let i = 0; i < N; i++) {
-    sum += arr[i] / N;
+for (let i = 0; i < N; i++) {
+  if (!map.has(input[i])) {
+    map.set(input[i], input[i].length);
   }
-  result.push(Math.round(sum));
-  // 중앙값
-  let centerArr = input;
-  centerArr.sort((a, b) => a - b);
-  result.push(centerArr[Math.floor(N / 2)]);
-  // 최빈값
-  // 셋째 줄에는 최빈값을 출력한다. 여러 개 있을 때에는 최빈값 중 두 번째로 작은 값을 출력한다.
-  let map = new Map();
-  for (let i = 0; i < N; i++) {
-    if (!map.has(input[i])) {
-      map.set(input[i], 1);
-    } else {
-      map.set(input[i], map.get(input[i]) + 1);
-    }
-  }
-  console.log(map.entries());
-  // 범위
-  result.push(input[input.length - 1] - input[0]);
 }
 
-stat(input);
+for (let entry of map) {
+  newArr.push(entry);
+}
 
-console.log(result);
+// 각 단어 길이로 정렬 후 사전 순으로 계산
+newArr.sort().sort((a, b) => {
+  if (a[1] !== b[1]) {
+    return a[1] - b[1];
+  }
+});
+
+for (let i = 0; i < newArr.length; i++) {
+  console.log(newArr[i][0]);
+}
