b350580698684f1b7cec8a6498f0b279224ff997
ko23yun <ko23yun@naver.com>
2021-09-25 11:41:49 +0900

    boj 1018 맞는데 왜 틀리지?

--- solve.js
+++ solve.js
@@ -146,13 +146,12 @@ let countArr = [];
 for (let i = 0; i < N - 7; i++) {
   for (let j = 0; j < M - 7; j++) {
     let chessEight = [];
-    for (let k = 0; k < N - 2; k++) {
-      for (let l = 0; l < M - 5; l++) {
+    for (let k = 0; k < 8; k++) {
+      for (let l = 0; l < 8; l++) {
         chessEight.push(chess[k + i][l + j]);
       }
     }
     // 여기서 확인
-    console.log(chessEight);
     let count = 0;
     if (chessEight[0] === "B") {
       for (let m = 0; m < 64; m++) {
@@ -171,6 +170,5 @@ for (let i = 0; i < N - 7; i++) {
   }
 }
 
-// countArr.sort((a, b) => a - b);
-
-console.log(countArr);
+countArr.sort((a, b) => a - b);
+console.log(countArr[0]);
