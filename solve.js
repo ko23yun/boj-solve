24d12be7a718a9331939c5c49d7d2c7d210d1fc3
ko23yun <ko23yun@naver.com>
2021-09-27 22:08:02 +0900

    boj 1018 다시 확인중..

--- solve.js
+++ solve.js
@@ -3,137 +3,25 @@ let fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
 let black = [
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
 ];
 
 let white = [
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
-  "B",
-  "W",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
 ];
 
 let NM = input.shift().split(" ");
@@ -143,32 +31,37 @@ let chess = input.map((word) => word.split(""));
 
 let countArr = [];
 
+// 배열을 [8개] ~ [8개] 이런식으로 만들어서 비교해보자
+
 for (let i = 0; i < N - 7; i++) {
   for (let j = 0; j < M - 7; j++) {
     let chessEight = [];
     for (let k = 0; k < 8; k++) {
+      let line = "";
       for (let l = 0; l < 8; l++) {
-        chessEight.push(chess[k + i][l + j]);
+        line += chess[k + i][l + j];
       }
+      chessEight.push(line);
     }
     // 여기서 확인
+    // 210927 각 배열 비교 및 확인하기
     let count = 0;
-    if (chessEight[0] === "B") {
-      for (let m = 0; m < 64; m++) {
-        if (chessEight[m] !== black[m]) {
-          count++;
-        }
-      }
+    if (chessEight[0][0] === "B") {
+      // for (let m = 0; m < 64; m++) {
+      //   if (chessEight[m] !== black[m]) {
+      //     count++;
+      //   }
+      // }
     } else {
-      for (let m = 0; m < 64; m++) {
-        if (chessEight[m] !== white[m]) {
-          count++;
-        }
-      }
+      // for (let m = 0; m < 64; m++) {
+      //   if (chessEight[m] !== white[m]) {
+      //     count++;
+      //   }
+      // }
     }
     countArr.push(count);
   }
 }
 
-countArr.sort((a, b) => a - b);
-console.log(countArr[0]);
+// countArr.sort((a, b) => a - b);
+// console.log(countArr[0]);
